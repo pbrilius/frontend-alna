3966051388ff353ecc51761824cb5461f77a951e
Povilas <pbrilius@gmail.com>
2021-01-16 15:23:29 +0200

    Dashboard & layout views in progress.

---
+++ resources/views/home/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+<h2>Content recognition</h2>
+@endsection()
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,14 @@
+<html>
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="/css/app.css" rel="stylesheet">
     <script src="/js/app.js"></script>
+    <title>@yield('title')</title>
 </head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
