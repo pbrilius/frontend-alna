b1d24963c03f4dabdef7f90a379a663fb96d92b5
Povilas <pbrilius@gmail.com>
2021-01-16 15:14:11 +0200

    Compiling Vue.js assets frontend.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+# pbrilius.medium.com
+Route::get('/dashboard', Dashboard::class);
+
+
