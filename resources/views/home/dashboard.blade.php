a9fc08439bbab77ee47e84183c895699c5722f06
Povilas <pbrilius@gmail.com>
2021-01-23 20:57:46 +0200

    Dashboard templte - flash messages hidden.

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    <div class="alert alert-warning" role="alert">
+    <div class="alert alert-warning d-none" role="alert">
     </div>
-    <div class="alert alert-success" role="alert">
+    <div class="alert alert-success d-none" role="alert">
     </div>
     <Dashboard></Dashboard>
     <Grid></Grid>
